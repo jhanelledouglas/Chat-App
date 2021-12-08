5955707fed23918782ecf4f88410ebdf053cfcde
jhanelledouglas <jhanelledouglas@yahoo.com>
2021-12-08 18:13:21 -0500

    Init commit

---
+++ public/js/chat.js
@@ -0,0 +1,137 @@
+//CLIENT SIDE OF APP
+
+const socket = io()
+    //Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Templates
+//innerHTML allows you To render HTML 
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sideBarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+//Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+
+const autoscroll = () => {
+    // New message element 
+    const $newMessage = $messages.lastElementChild
+
+    //Height of the last message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //visible Height
+    const visibleHeight = $messages.offsetHeight
+
+    //Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    //How far have I scrolled?
+
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+
+//MESSAGE LISTENER
+socket.on('message', (message) => {
+    console.log(message)
+        //Mustache allows you to get rendered html
+    const html = Mustache.render(messageTemplate, {
+
+        //message param is now an object so you need to access the text inside that object
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sideBarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
+
+//SUBMIT
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //DISABLES SUBMIT BUTTON
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+
+        //ENABLES DISABLED BUTTON
+        $messageFormButton.removeAttribute('disabled')
+
+        //CLEAR INPUT AFTER MESSAGE SEND
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message Delivered!')
+
+    })
+})
+
+
+//GEOLOCATION
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            //remove 
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')
+        })
+    })
+
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
